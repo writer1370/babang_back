68679c08d87bbe290e2daabd081a5ca76c3ec7b4
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2022-10-19 00:42:33 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/babang_back/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.project.babang_back.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MainController {
+    @GetMapping("/init/test")
+    public String init() {
+        return "hello world";
+    }
+}
---
+++ src/test/java/com/project/babang_back/BabangBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.babang_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabangBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
